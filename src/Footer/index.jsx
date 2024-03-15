ccfca3f366c31011a8e6089dbc26e14208816099
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-15 15:35:05 +0100

    The start of header, sidebar, and footer

---
+++ src/Footer/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+function Footer() {
+    return(
+        <footer>
+            <div>
+                <p>Hello, Footer!</p>
+                <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0&themeRefresh=1">Follow us</a>
+                <p>Copyright best group</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header/index.jsx
@@ -0,0 +1,13 @@
+import { IconDog } from '@tabler/icons-react'
+import './style.css'
+
+function Header() {
+    return(
+        <header>
+            <h1>Bob's Beagles</h1>
+            <IconDog></IconDog>
+        </header>
+    )
+}
+
+export default Header
