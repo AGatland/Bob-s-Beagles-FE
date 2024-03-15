f44c6210cfd9c69b42f8ae70289db9b13fa5c548
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-15 15:48:39 +0100

    Changes to footer

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -3,10 +3,15 @@ import './style.css'
 function Footer() {
     return(
         <footer>
-            <div>
+            <div className="footer-col">
                 <p>Hello, Footer!</p>
                 <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0&themeRefresh=1">Follow us</a>
                 <p>Copyright best group</p>
+            </div>
+            <div className="footer-col">
+                <p>Visit us:</p>
+                <p>123 Fake St</p>
+                <p>bobs@beagles.com</p>
             </div>
         </footer>
     )
