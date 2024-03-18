c16f8aa3ea498440a973875edb4fd14e8b63be7b
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-18 15:16:40 +0100

    Styling the product elements

--- src/ProductsList/Product/index.jsx
+++ src/ProductsList/Product/index.jsx
@@ -3,8 +3,8 @@ import './style.css'
 function Product({product}) {
     return(
         <div className="product-item">
-            <h4>{product.name}</h4>
             <img src={product.image_url} />
+            <h4>{product.name}</h4>
             <p>{product.price}</p>
             <button>Add to cart</button>
         </div>
