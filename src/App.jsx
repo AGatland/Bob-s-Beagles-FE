a3054f3ea13ff5f7805713c2eec8ffb95ad0599c
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-03-19 15:45:19 +0100

    Create login page without styling closes #17

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
-import './App.css'
-import Header from './Header'
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import Sidebar from './Sidebar';
-import { Route, Routes } from 'react-router-dom';
-import Footer from './Footer';
-import { createContext, useEffect, useState } from 'react';
-import Dashboard from './Dashboard';
-import { environment } from './environments/environment';
-import ProductsList from './ProductsList';
+import "./App.css";
+import Header from "./Header";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import Sidebar from "./Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./Footer";
+import { createContext, useEffect, useState } from "react";
+import Dashboard from "./Dashboard";
+import { environment } from "./environments/environment";
+import ProductsList from "./ProductsList";
+import Login from "./Login";
+import Signup from "./Signup";
 
-const ProductContext = createContext()
+const ProductContext = createContext();
 
 function App() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch(`${environment.apiUrl}products`)
-      .then(response => response.json())
-      .then(setProducts)
-  }, [])
+      .then((response) => response.json())
+      .then(setProducts);
+  }, []);
 
-  if (!products) return <h1>Loading products</h1>
+  if (!products) return <h1>Loading products</h1>;
 
   return (
     <MantineProvider>
-      <ProductContext.Provider value={{products: products}}>
+      <ProductContext.Provider value={{ products: products }}>
         <div className="container">
           <Header></Header>
           <div className="nav-main-container">
             <Sidebar></Sidebar>
             <Routes>
-              <Route
-                path="/"
-                element={<Dashboard />}
-                />
-              <Route
-                path="/products"
-                element={<ProductsList />}
-              />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/products" element={<ProductsList />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
             </Routes>
           </div>
           <Footer></Footer>
         </div>
       </ProductContext.Provider>
     </MantineProvider>
-  )
+  );
 }
 
-export {App, ProductContext}
+export { App, ProductContext };
--- src/Sidebar/index.jsx
+++ src/Sidebar/index.jsx
@@ -1,36 +1,44 @@
-import { Link } from 'react-router-dom'
-import './style.css'
-import { IconHome, IconDogBowl, IconUser, IconShoppingCart, IconSettings } from '@tabler/icons-react'
+import { Link } from "react-router-dom";
+import "./style.css";
+import {
+  IconHome,
+  IconDogBowl,
+  IconUser,
+  IconShoppingCart,
+  IconSettings,
+} from "@tabler/icons-react";
 
 function Sidebar() {
-    return(
-        <nav>
-            <Link to="/">
-                <button>
-                    <IconHome></IconHome>
-                    Home
-                </button>
-            </Link>
-            <Link to="/products">
-                <button>
-                    <IconDogBowl></IconDogBowl>
-                    Products
-                </button>
-            </Link>
-            <button>
-                <IconShoppingCart></IconShoppingCart>
-                Basket
-            </button>
-            <button>
-                <IconUser></IconUser>
-                Login
-            </button>
-            <button>
-                <IconSettings></IconSettings>
-                Settings
-            </button>
-        </nav>
-    )
+  return (
+    <nav>
+      <Link to="/">
+        <button>
+          <IconHome></IconHome>
+          Home
+        </button>
+      </Link>
+      <Link to="/products">
+        <button>
+          <IconDogBowl></IconDogBowl>
+          Products
+        </button>
+      </Link>
+      <button>
+        <IconShoppingCart></IconShoppingCart>
+        Basket
+      </button>
+      <Link to="/login">
+        <button>
+          <IconUser></IconUser>
+          Login
+        </button>
+      </Link>
+      <button>
+        <IconSettings></IconSettings>
+        Settings
+      </button>
+    </nav>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
