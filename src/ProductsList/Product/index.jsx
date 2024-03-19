70d996a1c846d717c9dd473685d0c080682cd8ca
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-19 11:56:42 +0100

    Addin already existing item to basket updates quantity number. ID is currently hardcoded in, this should come from elsewhere

--- src/ProductsList/Product/index.jsx
+++ src/ProductsList/Product/index.jsx
@@ -6,20 +6,54 @@ import { environment } from '../../environments/environment'
 /*
     TODO: When clicking add to basket, show confirmation. Possibly change button from add to basket to [-]<amount>[+] ?
         When adding existing item, update quantity
+        Replace placeholder values with userid
 */
 function Product({product}) {
     const basketContext = useContext(BasketContext)
 
     const handleClick = (event) => {
-        fetch(`${environment.apiUrl}basket`, {
-            method: 'POST',
-            header: [{'Content-Type': 'application/json'}],
-            body: JSON.stringify({
+        if (basketContext.basket.filter(b => b.sku === product.sku).length === 0)
+        {
+            fetch(`${environment.apiUrl}basket`, {
+                method: 'POST',
+                header: [{'Content-Type': 'application/json'}],
+                body: JSON.stringify({
+                    id: "123" + product.sku, /* To stop json-server from creating id */
+                    userId: "123",
+                    sku: product.sku,
+                    quantity: 1
+                })
+            })
+            basketContext.setBasket([...basketContext.basket, {
+                id: "123" + product.sku, /* To stop json-server from creating id */
                 userId: "123",
                 sku: product.sku,
                 quantity: 1
+            }]) 
+            console.log("baske now", basketContext.basket)
+        }
+        else {
+            /*
+                TODO: EDIT ID so it's not a hardcoded placeholder but comes from the url!!
+            */
+            let itemQuantity = basketContext.basket.filter(b => b.sku === product.sku)
+            fetch(`${environment.apiUrl}basket/123${product.sku}`, {
+                method: 'PUT',
+                header: [{'Content-Type': 'application/json'}],
+                body: JSON.stringify({
+                    id: "123" + product.sku, /* To stop json-server from creating id */
+                    userId: "123",
+                    sku: product.sku,
+                    quantity: itemQuantity[0].quantity + 1
+                })
             })
-        })
+            basketContext.setBasket([...basketContext.basket, {
+                id: "123" + product.sku, /* To stop json-server from creating id */
+                userId: "123",
+                sku: product.sku,
+                quantity: 1
+            }])
+        }
     }
 
     return(
