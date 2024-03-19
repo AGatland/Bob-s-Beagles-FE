232d44afca2ee9ee8b821eb6e78e6447dc3a5725
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-19 13:46:56 +0100

    Basket buttons work. Sorry the function is ugly

--- src/Basket/BasketList/BasketListItem/index.jsx
+++ src/Basket/BasketList/BasketListItem/index.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from "react"
-import { ProductContext } from "../../../App"
+import { BasketContext, ProductContext } from "../../../App"
 import { Loader } from "@mantine/core"
 import { environment } from "../../../environments/environment"
 
@@ -8,10 +8,89 @@ import { environment } from "../../../environments/environment"
     TODO: Make + and - buttons functional
         If multiple of same item in basket, show once with correct quantity number
         Price on the right should show singular, AND sum based on quantity
+        Add and remove copy pasted from Product. Have it elsewhere and pass throug context or props?
 */
 function BasketListItem({item}) {
+    const basketContext = useContext(BasketContext)
     const productContext = useContext(ProductContext)
     const [itemInfo, setItemInfo] = useState({})
+
+    const handleClick = (event) => {
+        if (event.target.name === "add") {
+            if (basketContext.basket.filter(b => b.sku === item.sku).length === 0)
+            {
+                fetch(`${environment.apiUrl}basket/123${product.sku}`, {
+                    method: 'POST',
+                    header: [{'Content-Type': 'application/json'}],
+                    body: JSON.stringify({
+                        id: "123" + item.sku, /* To stop json-server from creating id */
+                        userId: "123",
+                        sku: item.sku,
+                        quantity: 1
+                    })
+                })
+                basketContext.setBasket([...basketContext.basket, {
+                    id: "123" + item.sku, /* To stop json-server from creating id */
+                    userId: "123",
+                    sku: item.sku,
+                    quantity: 1
+                }]) 
+            }
+            else {
+                /*
+                    TODO: EDIT ID so it's not a hardcoded placeholder but comes from the url!!
+                */
+                let itemQuantity = basketContext.basket.filter(b => b.sku === item.sku)
+                fetch(`${environment.apiUrl}basket/123${item.sku}`, {
+                    method: 'PUT',
+                    header: [{'Content-Type': 'application/json'}],
+                    body: JSON.stringify({
+                        id: "123" + item.sku, /* To stop json-server from creating id */
+                        userId: "123",
+                        sku: item.sku,
+                        quantity: itemQuantity[0].quantity + 1
+                    })
+                })
+                basketContext.setBasket([...basketContext.basket, {
+                    id: "123" + item.sku, /* To stop json-server from creating id */
+                    userId: "123",
+                    sku: item.sku,
+                    quantity: 1
+                }])
+            }
+        } else if (event.target.name === "remove") {
+            if (basketContext.basket.filter(b => b.sku === item.sku)[0].quantity === 1)
+            {
+                fetch(`${environment.apiUrl}basket/123${item.sku}`, {
+                    method: 'DELETE',
+                    header: [{'Content-Type': 'application/json'}]
+                })
+                basketContext.setBasket(basketContext.basket.filter((basketItem) => basketItem.sku !== item.sku)) 
+            }
+            else {
+                /*
+                    TODO: EDIT ID so it's not a hardcoded placeholder but comes from the url!!
+                */
+                let itemQuantity = basketContext.basket.filter(b => b.sku === item.sku)
+                fetch(`${environment.apiUrl}basket/123${item.sku}`, {
+                    method: 'PUT',
+                    header: [{'Content-Type': 'application/json'}],
+                    body: JSON.stringify({
+                        id: "123" + item.sku, /* To stop json-server from creating id */
+                        userId: "123",
+                        sku: item.sku,
+                        quantity: itemQuantity[0].quantity - 1
+                    })
+                })
+                basketContext.setBasket([...basketContext.basket, {
+                    id: "123" + item.sku, /* To stop json-server from creating id */
+                    userId: "123",
+                    sku: item.sku,
+                    quantity: 1
+                }])
+            }
+        }
+    }
 
     useEffect(() => {
         setItemInfo(productContext.products.filter((p) => p.sku === item.sku))
@@ -23,7 +102,7 @@ function BasketListItem({item}) {
         <li className="basket-list-item">
             <p>{itemInfo[0].name}</p>
             <p>
-                <button name="remove">-</button> {item.quantity} <button name="add">+</button> {itemInfo[0].price}
+                <button name="remove" onClick={handleClick}>-</button> {item.quantity} <button name="add" onClick={handleClick}>+</button> {itemInfo[0].price}
             </p>
         </li>
     )
