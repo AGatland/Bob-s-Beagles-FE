38c65f9fce2d9ede8934a878342146ec0f56ea8f
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-20 13:04:03 +0100

    Fixed so you need to be authenticated. Closes #36

--- src/Dashboard/index.jsx
+++ src/Dashboard/index.jsx
@@ -1,15 +1,18 @@
-import { useContext } from "react"
-import { ProductContext } from "../App"
+import { useContext, useEffect } from "react"
+import { AuthContext, ProductContext } from "../App"
 import NewProducts from "./NewProducts"
 import './style.css'
 import { paragraph } from "../assets/placeholder"
 import { Loader } from "@mantine/core"
 import ShippingInfo from "./ShippingInfo"
+import { useNavigate } from "react-router-dom"
 
 function Dashboard() {
     const productContext = useContext(ProductContext)
+    const authContext = useContext(AuthContext)
+
     /* Display a loading circle if products not yet loaded */
-    if (!productContext.products) return <Loader color="blue" />;
+    if (!productContext.products && authContext.user) return <Loader color="blue" />;
 
     return(
         <div className="dashboard">
